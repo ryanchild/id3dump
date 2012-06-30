af482bb4cb83681aec65e28e9f2b78606482cecc
Ryan <ryan.child@gmail.com>
2012-06-30 17:59:24 -0400

    adding copyright stuff

--- id3dump.tcl
+++ id3dump.tcl
@@ -1,4 +1,37 @@
 #!/usr/bin/tclsh
+#
+# Copyright 2012 Ryan Child
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+proc PrintTags {arrlist} {
+  array set arr $arrlist
+  set colwidths [list 20 5]
+  set sep +
+  foreach w $colwidths {
+    append sep -[string repeat - $w]-+
+  }
+  set fmt "| %-[lindex $colwidths 0]s | %-[lindex $colwidths 1]s |"
+  puts $sep
+  puts [format $fmt "Field" "Value"]
+  puts $sep
+  foreach name [array names arr] {
+    puts [format $fmt $name $arr($name)]
+  }
+  puts $sep
+}
 
 if {$argc != 1} { 
   puts "usage: $argv0 filename"
@@ -10,19 +43,9 @@ if {[catch {set fd [open [lindex $argv 0]]} err]} {
   exit 1
 }
 
-set colwidths [list 20 5]
-set sep +
-foreach w $colwidths {
-  append sep -[string repeat - $w]-+
-}
-set fmt "| %-[lindex $colwidths 0]s | %-[lindex $colwidths 1]s |"
+binary scan [read $fd 3] "A3" val 
+set {values(ID3 Identifier)} $val
+binary scan [read $fd 2] "s" val 
+set {values(Major Version)} $val
+PrintTags [array get values]
 
-puts $sep
-puts [format $fmt "Field" "Value"]
-puts $sep
-
-binary scan [read $fd 3] "A3" id3_identifier 
-puts [format $fmt "ID3 Identifier" $id3_identifier]
-binary scan [read $fd 2] "s" major_version 
-puts [format $fmt "Major Version" $major_version]
-puts $sep
