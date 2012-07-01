951740e5a690ec05a66a788b2b507a09d41141e2
Ryan <ryan.child@gmail.com>
2012-06-30 20:31:17 -0400

    adding tag size, extended header extraction

--- id3dump.tcl
+++ id3dump.tcl
@@ -18,7 +18,7 @@
 
 proc PrintTags {arrlist} {
   array set arr $arrlist
-  set colwidths [list 20 5]
+  set colwidths [list 30 10]
   set sep +
   foreach w $colwidths {
     append sep -[string repeat - $w]-+
@@ -27,10 +27,16 @@ proc PrintTags {arrlist} {
   puts $sep
   puts [format $fmt "Field" "Value"]
   puts $sep
-  foreach name [array names arr] {
+  foreach name [lsort [array names arr]] {
     puts [format $fmt $name $arr($name)]
   }
   puts $sep
+}
+
+proc ErrNotID3 {} {
+  global argv
+  puts "File [lindex $argv 0] does not contain an ID3 tag"
+  exit 1
 }
 
 if {$argc != 1} { 
@@ -43,9 +49,39 @@ if {[catch {set fd [open [lindex $argv 0]]} err]} {
   exit 1
 }
 
-binary scan [read $fd 3] "A3" val 
-set {values(ID3 Identifier)} $val
-binary scan [read $fd 2] "s" val 
-set {values(Major Version)} $val
-PrintTags [array get values]
+binary scan [read $fd 3] A3 val 
+if {$val ne "ID3"} {ErrNotID3}
 
+set {values(File identifier)} $val
+binary scan [read $fd 1] c val 
+set {values(Major version)} $val
+binary scan [read $fd 1] c val
+set {values(Revision number)} $val
+binary scan [read $fd 1] c val
+set {values(Unsynchronisation)} [expr $val & 0x80]
+set {values(Extended header)} [expr $val & 0x40]
+set {values(Experimental indicator)} [expr $val & 0x20]
+
+# get ID3 tag size
+set bitstring "0000"
+set i 0
+while {$i < 4} {
+  binary scan [read $fd 1] B* tmp
+  if {[string index $tmp 0] ne "0"} {ErrNotID3}
+  append bitstring [string range $tmp 2 end]
+  incr i
+}
+binary scan [binary format B* $bitstring] I size
+set values(Size) $size
+
+# if there is an extended header, read it
+if {$values(Extended header)} {
+  binary scan [read $fd 4] I val
+  set {values(Extended header size)} $val
+  binary scan [read $fd 2] S val
+  set {values(Extended flags)} [expr $val & 0x8000]
+  binary scan [read $fd 4] I val
+  set {values(Size of padding)} $val
+}
+
+PrintTags [array get values]
