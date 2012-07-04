d47b8d438ce77408d7dd255ac97c5e0f32f860a8
Ryan <ryan.child@gmail.com>
2012-07-03 22:08:20 -0400

    - new data structure for holding tags in memory - can keep multiple tags open at once - adding option to dump cover art to file - adding option for verbose printing

--- id3dump.tcl
+++ id3dump.tcl
@@ -16,6 +16,208 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+proc shift {lst count} {
+  upvar $lst l
+  upvar $count c
+  set l [lreplace $l 0 0]
+  incr c -1
+}
+
+proc UsageString {} {
+  global argv0
+  # keep synced with ParseArgs
+  set optionalArgs [list --verbose --dump-cover]
+  set str "usage: $argv0 \[[join $optionalArgs "\] \["]\] file"
+  return $str
+}
+
+proc ParseArgs {} {
+  global argv gSettings
+  foreach arg $argv {
+    switch -exact -- $arg {
+      --verbose {
+        set gSettings(verbose) 1
+        shift argv argc
+      }
+      --dump-cover { 
+        set gSettings(dump_cover) 1
+        shift argv argc
+      }
+      default {
+        if {[string range $arg 0 0] eq "-"} {
+          error "unrecognized argument: $arg"
+        } else {
+          set gSettings(fname) $arg
+        }
+        shift argv argc
+        break
+      }
+    }
+  }
+  if {[llength $argv]} {
+    error "too many arguments"
+  }
+}
+
+proc GetSetting {s} {
+  global gSettings
+  if {[info exists gSettings($s)]} {
+    return $gSettings($s)
+  } else {
+    return 0
+  }
+}
+
+proc ID3_Size_From_Int {s} {
+  binary scan [binary format I $s] B32 bitstring
+  regexp .{4}(.{7})(.{7})(.{7})(.{7}) $bitstring m one two three four
+  binary format B32 "0${one}0${two}0${three}0${four}"
+}
+
+proc ID3_Size_To_Int {s} {
+  binary scan $s B32 bitstring
+  regexp .(.{7}).(.{7}).(.{7}).(.{7}) $bitstring m one two three four
+  binary scan [binary format B32 "0000${one}${two}${three}${four}"] I ret
+  return $ret
+}
+
+proc ID3_Read_Header {fd} {
+  global gID3Data
+  set data [read $fd 10]
+
+  binary scan $data A3 ident 
+  binary scan $data @5c flags
+  binary scan $data @6I size
+  if {$ident ne "ID3" || $size & 0x80808080} {
+    error "valid ID3 header not found"
+  }
+
+  set extendedHeader [expr $flags & 0x40]
+  if {$extendedHeader} {
+    set gID3Data($fd,extendedheader) [read $fd 10]
+    incr gID3Data($fd,size) 10
+  }
+
+  set sizebytes [string range $data end-3 end]
+  set gID3Data($fd,sizeInHeader) [ID3_Size_To_Int $sizebytes]
+  set gID3Data($fd,header) $data
+}
+
+proc ID3_Frame_Add {fd id size flags data} {
+  global gID3Data
+  set gID3Data($fd,$gID3Data($fd,currHandle),id) $id
+  set gID3Data($fd,$gID3Data($fd,currHandle),size) $size
+  set gID3Data($fd,$gID3Data($fd,currHandle),flags) $flags
+  set gID3Data($fd,$gID3Data($fd,currHandle),data) $data
+  set gID3Data($fd,h$id) $gID3Data($fd,currHandle)
+  incr gID3Data($fd,size) [expr $size + 10]
+  incr gID3Data($fd,currHandle)
+}
+
+proc ID3_Open {fname} {
+  global gID3Data 
+  set fd [open $fname]
+  fconfigure $fd -translation binary
+  set gID3Data($fd,currHandle) 0
+  set gID3Data($fd,size) 0
+
+  ID3_Read_Header $fd
+  while {$gID3Data($fd,size) < $gID3Data($fd,sizeInHeader)} {
+    binary scan [read $fd 4] A* id
+    binary scan [read $fd 4] I size
+    binary scan [read $fd 2] S flags
+    set data [read $fd $size]
+    ID3_Frame_Add $fd $id $size $flags $data
+    if {[GetSetting verbose]} {
+      puts "Read $id frame: $size bytes"
+    }
+  }
+  if {[GetSetting verbose]} {
+    puts "ID3 Size: $gID3Data($fd,sizeInHeader) (read $gID3Data($fd,size) bytes)"
+  }
+  return $fd
+}
+
+proc ID3_Close {fd} {
+  global gID3Data
+  for {set i 0} {$i < $gID3Data($fd,currHandle)} {incr i} {
+    if {[info exists gID3Data($fd,h$gID3Data($fd,$i,id))]} {
+      unset gID3Data($fd,h$gID3Data($fd,$i,id))
+    }
+    unset gID3Data($fd,$i,id)
+    unset gID3Data($fd,$i,size)
+    unset gID3Data($fd,$i,flags)
+    unset gID3Data($fd,$i,data)
+  }
+  unset gID3Data($fd,size)
+  unset gID3Data($fd,sizeInHeader)
+  unset gID3Data($fd,currHandle)
+  close $fd
+}
+
+proc ID3_Write_Frame {fd h} {
+  global gID3Data
+  puts $fd [binary format a4 $gID3Data($h,id)]
+  puts $fd [binary format I $gID3Data($h,size)]
+  puts $fd [binary format S $gID3Data($h,flags)]
+  puts $fd $gID3Data($h,data)
+}
+
+proc ID3_Num_Frames {fd} {
+  global gID3Data
+  return $gID3Data($fd,currHandle)
+}
+
+proc ID3_Write_Frames {fd} {
+  global gID3Data
+  for {set i 0} {$i < [ID3_Num_Frames $fd]} {incr i} {
+    ID3_Frame_Write $fd $i
+  }
+}
+
+proc ID3_Get_Data {fd frameid} {
+  global gID3Data
+  set handle $gID3Data($fd,h$frameid)
+  return $gID3Data($fd,$handle,data)
+}
+
+proc ID3_Get_Text {fd frameid} {
+  if {[string range $frameid 0 0] ne "T"} {
+    error "only able to get text values from frame IDs that start with 'T'"
+  }
+  binary scan [ID3_Get_Data $fd $frameid] A* txt
+  return $txt
+}
+
+proc ID3_Print_Header {fd} {
+  global gID3Data
+  upvar 0 $gID3Data($fd,header) data
+
+  binary scan $data @3c major 
+  binary scan $data @4c rev
+  binary scan $headerData @5c flags
+  set unsync [expr $flags & 0x80]]
+  set extendedHeader [expr $flags & 0x40]
+  set experimental [expr $flags & 0x20]
+  set size [ID3_Size_To_Int [string range $data end-4 end]]
+}
+
+proc ID3_Dump_Artwork {fd} {
+  set data [ID3_Get_Data $fd APIC]
+
+  set idx 1
+  set mime [lindex [split [string range $data $idx end] \x00] 0]
+  incr idx [expr [string length $mime] + 1]
+  set desc [lindex [split [string range $data  $idx end] \x00] 0]
+  incr idx [expr [string length $desc] + 1]
+
+  set fname cover.[string range $mime [expr [string first / $mime] + 1] end]
+  set out [open $fname w]
+  fconfigure $out -translation binary -encoding binary
+  puts $out [string range $data $idx end]
+  close $out
+}
+
 proc InitTable {} {
   global gTableSep gTableFmt gTableWidth
   set colWidths {30 30}
@@ -65,92 +267,25 @@ proc PrintTable {title rows} {
   PrintTableFooter
 }
 
-proc ErrNotID3 {} {
-  global argv
-  puts "File [lindex $argv 0] does not contain an ID3 tag"
+if {[catch {ParseArgs} err]} {
+  puts stderr $err
+  puts stderr [UsageString]
   exit 1
 }
 
-proc ReadID3Data {fd bytes} {
-  global gBytesRead gID3Size
-  if {![info exists gBytesRead]} {
-    set gBytesRead 0
-  }
-  if {[info exists gID3Size] && $gBytesRead >= $gID3Size - 10} {
-    return 0
-  }
-  incr gBytesRead $bytes
-  read $fd $bytes
-}
-
-proc ReadFrame {fd id3frame} {
-  upvar $id3frame f
-  set data [ReadID3Data $fd 4]
-  if {$data == 0} {
-    return 0
-  }
-  binary scan $data A* frameid
-  lappend f [list "Frame ID" $frameid]
-  binary scan [ReadID3Data $fd 4] I size
-  lappend f [list "Size" $size]
-  binary scan [ReadID3Data $fd 2] S flags
-  ReadID3Data $fd $size
-  lappend f [list "Data" "<binary data>"]
-  return 1
-}
-
-if {$argc != 1} { 
-  puts stderr "usage: $argv0 filename"
-  exit 1
-}
-
-if {[catch {set fd [open [lindex $argv 0]]} err]} {
+if {[catch {set id3 [ID3_Open [GetSetting fname]]} err]} {
   puts stderr $err
   exit 1
 }
-fconfigure $fd -translation binary
 
-InitTable
+puts {}
+puts "Album: [ID3_Get_Text $id3 TALB]"
+puts "Soloist: [ID3_Get_Text $id3 TPE1]"
+puts "Band: [ID3_Get_Text $id3 TPE2]"
+puts "Title: [ID3_Get_Text $id3 TIT2]"
 
-# read file identifier
-binary scan [ReadID3Data $fd 3] A3 val 
-if {$val ne "ID3"} {ErrNotID3}
-lappend id3header [list "File identifier" $val]
-
-# read version info
-binary scan [ReadID3Data $fd 1] c major 
-binary scan [ReadID3Data $fd 1] c rev
-lappend id3header [list "Version" ${major}.${rev}]
-# read header flags
-binary scan [ReadID3Data $fd 1] c flags
-lappend id3header [list "Unsynchronization" [expr $flags & 0x80]]
-set extendedHeader [expr $flags & 0x40]
-lappend id3header [list "Extended header" $extendedHeader]
-lappend id3header [list "Experimental indicator" [expr $flags & 0x20]]
-
-# get ID3 tag size
-set bitstring "0000"
-for {set i 0} {$i < 4} {incr i} {
-  binary scan [ReadID3Data $fd 1] B* tmp
-  if {[string index $tmp 0] ne "0"} {ErrNotID3}
-  append bitstring [string range $tmp 1 end]
-}
-binary scan [binary format B* $bitstring] I gID3Size
-lappend id3header [list "Size" $gID3Size]
-
-# if there is an extended header, read it
-if {$extendedHeader} {
-  binary scan [ReadID3Data $fd 4] I val
-  lappend extheader [list "Extended header size" $val]
-  binary scan [ReadID3Data $fd 2] S val
-  lappend extheader [list "Extended flags" [expr $val & 0x8000]]
-  binary scan [ReadID3Data $fd 4] I val
-  lappend extheader [list "Size of padding" $val]
+if {[GetSetting dump_cover]} {
+  ID3_Dump_Artwork $id3
 }
 
-PrintTable "ID3 Header" $id3header
-set i 1
-while {[ReadFrame $fd id3frame$i]} {
-  PrintTable "Frame #$i" [set id3frame$i]
-  incr i
-}
+ID3_Close $id3
